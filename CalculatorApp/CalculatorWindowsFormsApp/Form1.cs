c7ff2b931a4749d382ee2ee4be78a27cdb5175b0
mfhumam <128024369+mfhumam@users.noreply.github.com>
2023-09-14 10:20:45 +0700

    Add files via upload

---
+++ CalculatorApp/CalculatorWindowsFormsApp/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CalculatorMachine;
+
+namespace CalculatorWindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Calculator cal = new Calculator();
+
+        private void btnHitung_Click(object sender, EventArgs e)
+        {
+            var a = int.Parse(txtNilaiA.Text); // casting
+            var b = Convert.ToInt32(txtNilaiB.Text); // casting
+
+            lstHasil.Items.Clear(); // kosongkan isi listbox
+
+            // tampilkan hasil semua hasil pemanggilan method calculator ke dalam listbox
+            lstHasil.Items.Add(string.Format("Hasil Penambahan: {0} + {1} = {2}", a, b, cal.Penambahan(a, b)));
+            lstHasil.Items.Add(string.Format("Hasil Pengurangan: {0} - {1} = {2}", a, b, cal.Pengurangan(a, b)));
+            lstHasil.Items.Add(string.Format("Hasil Perkalian: {0} x {1} = {2}", a, b, cal.Perkalian(a, b)));
+            lstHasil.Items.Add(string.Format("Hasil Pembagian: {0} / {1} = {2}", a, b, Calculator.Pembagian(a, b)));
+            lstHasil.Items.Add(string.Format("Hasil Perpangkatan: {0} ^ {1} = {2}", a, b, cal.Pangkat(a, b)));
+            lstHasil.Items.Add(string.Format("Hasil Modulo: {0} % {1} = {2}", a, b, Calculator.Modulo(a, b)));
+        }
+
+        private void txtNilaiA_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
